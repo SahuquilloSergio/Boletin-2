ce8ad1ec5e6f05cc7534998f617c6cc9c838647d
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-10-26 09:41:20 +0200

    Commit Inicial

---
+++ Boletin2_2/src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,17 @@
+package boletin2_2;
+
+public class Boletin2_2 {
+
+    public static void main(String[] args) {
+       
+       int lado = 3;
+       System.out.println("area = "+ lado*lado);
+               
+        
+        
+        
+        
+        
+    }
+    
+}
