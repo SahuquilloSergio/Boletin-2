d09026e14210fe5f069e204e8e4a9090dc523788
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-10-26 09:42:00 +0200

    Commit Inicial

---
+++ Boletin2_4_1/src/boletin2_4/Boletin2_4.java
@@ -0,0 +1,26 @@
+package boletin2_4;
+import java.util.Scanner;
+public class Boletin2_4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float n1, n2, suma, resta, producto, cociente;
+        
+        System.out.println(" teclea el primer numero :");
+        n1 = sc.nextFloat();
+        System.out.println(" teclea el segundo numero :");
+        n2 = sc.nextFloat();
+        System.out.println("resultado suma = "+ (n1+n2)+"   resultado resta = "+ (n1 - n2)+"   resultado producto = "+ (n1 * n2)+"   resultado division = "+ (n1 / n2));
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
